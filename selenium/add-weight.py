c0c8f824aba78212744e720ca2877c41df60ddc9
Chris Arroyo <carroyo@saltstack.com>
2016-01-06 11:34:36 -0700

    add_weight automation

---
+++ selenium/add-weight.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import unittest, time, re
+
+class AddWeight(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(30)
+        self.base_url = "http://arroyobox.com/"
+        self.verificationErrors = []
+        self.accept_next_alert = True
+    
+    def test_add_weight(self):
+        driver = self.driver
+        driver.get(self.base_url + "/")
+        driver.find_element_by_name("username").clear()
+        driver.find_element_by_name("username").send_keys("test")
+        driver.find_element_by_name("password").clear()
+        driver.find_element_by_name("password").send_keys("dilbert")
+        driver.find_element_by_xpath("//button[@name='']").click()
+
+
+        for x in range(5):
+            weight_button = driver.find_element_by_css_selector("button.btn.btn-primary").click()
+            weight_box_clear = driver.find_element_by_id("weight").clear()
+            weight_box_insert = driver.find_element_by_id("weight").send_keys("100")
+            weight_box_submit = driver.find_element_by_xpath("(//input[@type='submit'])[2]").click()
+
+            weight_button
+            weight_box_clear
+            weight_box_insert
+            weight_box_submit
+
+    
+    def is_element_present(self, how, what):
+        try: self.driver.find_element(by=how, value=what)
+        except NoSuchElementException as e: return False
+        return True
+    
+    def is_alert_present(self):
+        try: self.driver.switch_to_alert()
+        except NoAlertPresentException as e: return False
+        return True
+    
+    def close_alert_and_get_its_text(self):
+        try:
+            alert = self.driver.switch_to_alert()
+            alert_text = alert.text
+            if self.accept_next_alert:
+                alert.accept()
+            else:
+                alert.dismiss()
+            return alert_text
+        finally: self.accept_next_alert = True
+    
+    def tearDown(self):
+        self.driver.quit()
+        self.assertEqual([], self.verificationErrors)
+
+if __name__ == "__main__":
+    unittest.main()
